5d9fc0be9a098bf932a6dd8d46fbd11612f99bda
Sankalp-G <sankalp.gawali@gmail.com>
2024-12-07 15:22:47 +0530

    feat: fix svg attributes

--- frontend/src/components/loader.tsx
+++ frontend/src/components/loader.tsx
@@ -4,10 +4,10 @@ export default function Loader() {
       <path
         fill="none"
         stroke="#5F9EA0"
-        stroke-width="12"
-        stroke-linecap="round"
-        stroke-dasharray="300 385"
-        stroke-dashoffset="0"
+        strokeWidth="12"
+        strokeLinecap="round"
+        strokeDasharray="300 385"
+        strokeDashoffset="0"
         d="M275 75c0 31-27 50-50 50-58 0-92-100-150-100-28 0-50 22-50 50s23 50 50 50c58 0 92-100 150-100 24 0 50 19 50 50Z"
       >
         <animate
